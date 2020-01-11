cb8f3075881d7036f2ef1c4bd6b1c5966315db95
Gregmar Cruz <gregr1201@gmail.com>
2020-01-11 01:26:02 -0500

    Passport JWT

--- app.js
+++ app.js
@@ -19,7 +19,8 @@ const mongoose = require('mongoose');
 
 const Dishes = require('./models/dishes');
 
-const url = 'mongodb://localhost:27017/conFusion';
+var config = require('./config');
+const url = config.mongoUrl;
 const connect = mongoose.connect(url);
 
 connect.then((db) => {
@@ -38,34 +39,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 //app.use(cookieParser('12345-67890-09876-54321'));
 
-app.use(session({
-    name: 'session-id',
-    secret: '12345-67890-09876-54321',
-    saveUninitialized: false,
-    resave: false,
-    store: new FileStore()
-}));
-
 app.use(passport.initialize());
-app.use(passport.session());
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-function auth (req, res, next) {
-    console.log(req.user);
-
-    if (!req.user) {
-      var err = new Error('You are not authenticated!');
-      err.status = 403;
-      next(err);
-    }
-    else {
-          next();
-    }
-}
-
-app.use(auth);
+app.use('/users', usersRouter); 
 
 app.use(express.static(path.join(__dirname, 'public')));
 
