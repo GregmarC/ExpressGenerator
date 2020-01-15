b98e62fd5d8fcf34a292cbc4450d1cc4d11259a6
Gregmar Cruz <gregr1201@gmail.com>
2020-01-15 01:04:17 -0500

    File Upload

--- app.js
+++ app.js
@@ -18,6 +18,8 @@ var authenticate = require('./authenticate');
 const mongoose = require('mongoose');
 
 const Dishes = require('./models/dishes');
+const uploadRouter = require('./routes/uploadRouter');
+
 
 var config = require('./config');
 const url = config.mongoUrl;
@@ -60,6 +62,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/dishes',dishRouter);
 app.use('/promotions',promoRouter);
 app.use('/leaders',leaderRouter);
+app.use('/imageUpload',uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
