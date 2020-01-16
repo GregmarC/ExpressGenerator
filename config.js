173c5db0d6082475f7d09a792630e05198c947d7
Gregmar Cruz <gregr1201@gmail.com>
2020-01-16 14:54:37 -0500

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,9 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook' : {
+        clientId: '940229823037982',
+        clientSecret: 'b6e56fb2d29a922bb826915f3b9a8b56'
+    }
 }
+
